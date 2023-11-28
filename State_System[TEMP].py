a6ae9857c3901562c56d831aa675ad931c5e8c93
IliasFransen <127627710+IliasFransen@users.noreply.github.com>
2023-11-28 11:44:19 -0600

    added solver for ODE

--- State_System[TEMP].py
+++ State_System[TEMP].py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.integrate import solve_ivp
 from FBD_Stuff import Get_Angles
 from Modified_Newton import Get_Normal
 
@@ -7,8 +8,8 @@ from Modified_Newton import Get_Normal
 def StateUpdate(t : list, states: list, g : float, m : float, alpha : list, gamma : float, x_lst : list, y_lst : list):
     Vx, Vy, h = states
 
-    eta = Get_Angles(Vx, Vy, alpha)
-    N = Get_Normal(Vx, Vy, h, gamma, x_lst, y_lst, alpha)
+    eta = Get_Angles(Vx, Vy, alpha(t))
+    N = Get_Normal(Vx, Vy, h, gamma, x_lst, y_lst, alpha(t))
 
     dVxdt = N*np.cos(eta-np.pi)/m
     dVydt = -g + N*np.sin(eta-np.pi)/m
@@ -28,11 +29,11 @@ def Get_VInit (V0 : float, eta0 : float):
     return Vx0, Vy0
 
 
-def Solver(V0 : float, eta0 : float, h0 : float, t : list, g : float, m : float, alpha : list, gamma : float, x_lst : list, y_lst : list):
+def Solver(V0 : float, eta0 : float, h0 : float, t0 : float, t_end : float, g : float, m : float, alpha : list, gamma : float, x_lst : list, y_lst : list):
     Vx0 , Vy0 = Get_VInit(V0, eta0)
     State0 = [Vx0, Vy0, h0]
 
     param = (g, m, alpha, gamma, x_lst, y_lst) #doubt I can set array as a parameter
 
-
+    sol = solve_ivp(StateUpdate,(t0,t_end) ,State0, param )
 
