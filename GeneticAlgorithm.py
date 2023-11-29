92ed129c2690080a43d57e14cb0bfe2c2ef7e109
skilduff <samk3@illinois.edu>
2023-11-29 17:14:25 -0600

    some edits

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -1,28 +1,32 @@
-from inspect import _void
 import numpy as np
 import random
 from deap import base, creator, tools, algorithms
+from Atmosphereic_Conditions import Get_Density
+from Modified_Newton import Get_Normal
 
 class GeneticAlgorithmOptimization:
            
     # GA setup
 
-    ngen = 10 # number of generations
-    nind = 10 # number of individuals
+    ngen = 5 # number of generations
+    nind = 5 # number of individuals
     eta = 5.0 
-    MUTPB = 0.5 # probability of mutation
-    CXPB = 0.5 # probability of crossover
-    hof = 5 # hall of fame size
+    mutpb = 0.5 # probability of mutation
+    cxpb = 0.5 # probability of crossover
+    nhof = 5 # hall of fame size
     
     # control variable bounds
 
-    alpha_bounds = np.array([0, 2 * np.pi / 180]) # AoA bounds [rad] 
+    alpha_bounds = [0, 2 * np.pi / 180] # AoA bounds [rad] 
     alpha_min = alpha_bounds[0]
     alpha_max = alpha_bounds[1]
     
     # constraints
 
-    q_max = 45 # max. stag. heat [W/cm^3]
+    q_max = 45 * 10**6 # max. stag. heat [W/m^2]
+    
+    # Sutton-Graves stagnation point heat transfer coefficient for earth
+    k = 1.7415*10**(-4)
 
     def __init__(self):
 
@@ -30,32 +34,38 @@ class GeneticAlgorithmOptimization:
         alpha_max = self.alpha_max
         eta = self.eta
         
-        creator.create("FitnessMin", base.Fitness, weights=(-1.0))
+        creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
         creator.create("Individual", list, fitness=creator.FitnessMin)
         
         self.toolbox = base.Toolbox()
         toolbox = self.toolbox
-        evaluate = self.evalulate        
+        evaluate = self.evaluate        
         check_feasibility = self.check_feasibility
-        penalty = self.penalty
 
         toolbox.register("alpha", random.uniform, alpha_min, alpha_max)
         toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.alpha, n = 1)
         toolbox.register("population", tools.initRepeat, list, toolbox.individual)
         toolbox.register("evaluate", evaluate)
         toolbox.register("mate", tools.cxSimulatedBinaryBounded, low=alpha_min, up=alpha_max, eta=eta)
-        toolbox.mutate("mutate", tools.mutPolynomialBounded, low=alpha_min, up=alpha_max, eta=eta, indpb=0.08)
+        toolbox.register("mutate", tools.mutPolynomialBounded, low=alpha_min, up=alpha_max, eta=eta, indpb=0.08)
         toolbox.register("select", tools.selNSGA2)    
-        toolbox.decorate("evaulate", tools.DeltaPenalty(check_feasibility, 1000, penalty))
+        toolbox.decorate("evaluate", tools.DeltaPenalty(check_feasibility, 1000))
         
     def solve(self, alpha):
       
+        """
         rho0 = self.atm_params[0]
         beta = 6700 # scale height [m]
         z = self.x[0] # height [m]
         rho = rho0 * np.exp(-beta * z) # density at height [kg/m^3]
+        """
+        
+        h = self.x[2]
 
-        V = self.x[1]
+        rho = Get_Density(h).tolist()[0]
+        
+        Vx = self.x[0]
+        Vy = self.x[1]
 
         R0 = self.sc_params[0]
         S = self.sc_params[1]
@@ -70,26 +80,37 @@ class GeneticAlgorithmOptimization:
         L = 0.5 * rho * pow(V, 2) * Cl * S
         """
         
+        # Some BS calculations i know they arent right was just testing GA
         Cp = 2 * pow(np.sin(alpha), 2) # coeff. of pressure Newtonian theory
+        V = np.sqrt(Vx**2 + Vy**2) # velocity magnitude
         q_dyn = 0.5 * rho * pow(V, 2) # dynamic pressure [Pa]
         
         N = Cp * q_dyn * S # normal force [N]
 
-        q = self.k * np.sqrt((self.rho / self.R0)) * pow(self.V, 3) # Sutton-Graves stagnation point heat flux approximation [W/m^2]
+        
+        #N = Get_Normal(Vx, Vy, h, gamma, x_lst, y_lst, alpha)
+        
+        q = self.k * np.sqrt((rho / R0)) * pow(V, 3) # Sutton-Graves stagnation point heat flux approximation [W/m^2]
         
         # add other constraints here if needed
         
         return [alpha, N, q]
 
-    def getSolution(self, x, atm_params, sc_params):
+    def getSolution(self, x, alpha, atm_params, sc_params):
          
-        # Inputs x = [h, ]
+        # state x = [h, Vx, Vy]
 
-        [h, v] = x[-1]
-        alpha = 1
+        [Vx, Vy, h] = x[-1]
+        
         [R0, S] = sc_params
+        
+        [rho0] = atm_params
+        
+        self.x = x[-1]
+        self.sc_params = sc_params
+        self.atm_params = atm_params
 
-        hof = self.solve() 
+        hof = self.optimize() 
 
         alpha = hof[0][0]
         
@@ -97,34 +118,49 @@ class GeneticAlgorithmOptimization:
 
         return sol
     
+    # Evolutionary algorithm
+
     def optimize(self):
         
         toolbox = self.toolbox
         pop = toolbox.population(n = self.nind)
         NGEN = self.ngen
-        CXPB = self.CXPB
-        MUTPB = self.MUTPB    
-        hof = self.hof
+        CXPB = self.cxpb
+        MUTPB = self.mutpb    
+        hof = tools.HallOfFame(self.nhof)
 
         algorithms.eaSimple(pop, toolbox, cxpb=CXPB, mutpb=MUTPB, ngen=NGEN, halloffame=hof, verbose=True)
         
         return hof
     
+    # Check feasibility of solution
+
     def check_feasibility(self, params):
         
         alpha = params[0]
 
-        q = self.getSolution(alpha)[3]
+        q = self.solve(alpha)[2]
 
         if q > self.q_max:
             return False
         else:
             return True
         
-    def evaluate(self):
+    # Evaluate solution with objective function
+
+    def evaluate(self, params):
         
-        weights = [0.5, 0.5]
+        alpha = params[0]
+
+        weights = [0.5]
         
-    def penalty(self):
+        sol = self.solve(alpha)
+        alpha = sol[0]
+        q = sol[2]
+
+        objectives = [q]
         
-        return True
+        objfcn = sum(x * y for x, y in zip(weights, objectives)) / sum(weights) # weighted sum average objective function
+        
+        return objfcn,
+        
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from GeneticAlgorithm import GeneticAlgorithmOptimization
+#from State_System[TEMP] import StateUpdate
+from scipy.integrate import solve_ivp
+from math import atan2
 
 # Atmosphere parameters
 rho0 = 1.225 # density of air at sea level [kg/m^3]
@@ -9,19 +13,38 @@ atm_params = [rho0]
 # Spacecraft parameters (temp values)
 R0 = 0.25 # nose radius (m)
 S = 0.8 # reference area [m^2]
+m = 2000 # mass [kg]
 
 sc_params = [R0, S]
 
 # ICs (temps values)
-h0 = 100000 # initial altitude [m]
-Vx0 = 100000 # initial x velocity [m/s]
-Vy0 = 100000 # initial y velocity [m/s]
+h0 = 80000 # initial altitude [m]
+Vx0 = 0 # initial x velocity [m/s]
+Vy0 = -1800 # initial y velocity [m/s]
 
-ics = [Vx0, Vy0, h0] 
+ICs = [Vx0, Vy0, h0] # initial conditions vector 
 
 # control var
 
 alpha = 0 # AoA [rad], to be optimized
+
+g = 9.81
+
+def EOMs(t, x, *args):
+    
+    alpha, N = args
+    
+    [Vx, Vy, h] = x
+    
+    Beta = atan2(Vy, Vx)
+    
+    eta = Beta - alpha
+
+    dVx_dt = N * np.cos(eta - np.pi) / m
+    dVy_dt = -g + N * np.sin(eta - np.pi) / m
+    dh_dt = Vy
+    
+    return [dVx_dt, dVy_dt, dh_dt]
 
 if __name__ == "__main__":
     
@@ -30,20 +53,42 @@ if __name__ == "__main__":
     t = np.arange(0, 10, 1)
     
     x = []
-    x.append(ics)
+    x.append(ICs)
 
-    for i in range(0, len(t)):
+    for i in range(0, len(t) - 1):
         
-        x = ics[-1]
+        t1 = t[i]
+        t2 = t[i+1]
+        tspan = [t1, t2]
 
-        opt = GA.getSolution() # run genetic algorithm to get optimum
-                
+        opt = GA.getSolution(x, alpha, atm_params, sc_params) # run genetic algorithm to get optimum
+        
         alpha = opt[0]
         N = opt[1]
         
         # ODE stuff with optimum
 
-        sol = []
+        x0 = x[-1] # initialize with previous solution
         
-        x.append(sol)
+        args = [alpha, N]
+
+        sol = solve_ivp(EOMs, tspan, x0, args=args)
         
+        x.append( [sol.y[0][-1], sol.y[1][-1], sol.y[2][-1]] ) 
+            
+        
+    x = list(zip(*x))
+    
+    Vx = x[0]
+    Vx2 = tuple(x**2 for x in Vx)
+    Vy = x[1]
+    Vy2 = tuple(x**2 for x in Vy)
+    V = tuple(np.sqrt(x + y) for x,y in zip(Vx2, Vy2))
+    h = x[2]
+
+    plt.figure(1)
+    plt.plot(t, h)
+    
+    plt.figure(2)
+    plt.plot(V, h)
+    plt.show()
