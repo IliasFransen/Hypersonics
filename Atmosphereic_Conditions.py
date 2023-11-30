c6042e4ae007bf3a5c3cb4f936e5c050d9b3e4f2
IliasFransen <127627710+IliasFransen@users.noreply.github.com>
2023-11-30 15:31:18 -0600

    Update Atmosphereic_Conditions.py
    changed to isacalc

--- Atmosphereic_Conditions.py
+++ Atmosphereic_Conditions.py
@@ -1,16 +1,18 @@
 import numpy as np
-from ambiance import Atmosphere as atmos
+import isacalc as isa
 
 #Get_SoundSpeed takes the altitude in meters as a float and returns the speed of sound at said altitude in meters per second
 
 def Get_SoundSpeed (h: float):
-    a = atmos(h).speed_of_sound
+    atmosphere = isa.get_atmosphere()
+    T, P, d, a, mu = isa.calculate_at_h(h, atmosphere)
     return float(a)
 
 
 #Get_Density gives the density at a certain altitiude
 
 def Get_Density(h: float):
-    rho = atmos(h).density
+    atmosphere = isa.get_atmosphere()
+    T, P, rho, a, mu = isa.calculate_at_h(h, atmosphere)
     return float(rho)
 
